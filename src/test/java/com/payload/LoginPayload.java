05ab5fa0e95330a49fd1d4020ec6c0b17f8a1856
AntarikCodes <sinhaantarik@gmail.com>
2026-04-08 19:48:31 +0530

    Add:Config Reaer

--- src/test/java/com/payload/LoginPayload.java
+++ src/test/java/com/payload/LoginPayload.java
@@ -2,7 +2,7 @@ package com.payload;
 
 public class LoginPayload {
     public static String login(){
-        String data="{\n" + "  \"email\": \"13564@example.com\",\n" + "  \"password\": \"secret123\"\n" +"}";
+        String data="{\n" + "  \"email\": \"065@example.com\",\n" + "  \"password\": \"secret123\"\n" +"}";
         return data;
     }
 }
